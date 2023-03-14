81ab87a21bfcb966018d443ffbdf199558c7496a
Bhanu Pratap Malviya <113906324+bhanupratap70@users.noreply.github.com>
2023-03-14 18:25:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let input = document.getElementById('inputbox');
+let buttons = document.querySelectorAll
+('button');
+
+let string = "";
+let arr = Array.from('buttons');
+arr.forEach(button =>{
+  button.addEvenetListener('click', (e) => {
+    if(e.target.innerHTML == '='){
+
+    }
+  }
+
+}
+ 
